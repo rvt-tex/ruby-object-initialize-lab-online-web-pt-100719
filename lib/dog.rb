b6fb6dbdfd318f99c132184047f95418e3d41637
Ruel Tingling <rvtingling@gmail.com>
2019-10-14 21:42:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   def initialize(name, breed)
     @name = name
-    @breed = mutt
+    @breed = breed
   end
  
   def name=(name)
@@ -13,7 +13,7 @@ class Dog
   end
 
  def breed=(breed)
-    @breed = breed
+    @breed = mutt
   end
  
   def breed
