ee50893952c9bc05f3680240235623f793235e65
Ruel Tingling <rvtingling@gmail.com>
2019-10-14 22:02:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,4 +27,6 @@ end
 
 
 # in line one we creating a dog class by using (class)
-# in line two we are initializing the name and breed of the dog class, while setting 
+# in line two we are initializing the name and breed of the dog class, while setting while setting and optional arguement for the dogs breed.
+#line three and four we have to create the instance variable use @ for both arguements ( name and breed)
+# in line seven we have to define instance variable and passing both arguements (name, breed = "default name given")
